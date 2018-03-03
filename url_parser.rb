7b461f9bb592eea121e87010b017b1ee159e0b75
Glynis Monroe <glynis.d.monroe@gmail.com>
2018-03-03 19:24:25 +0000

    url_parser project

---
+++ url_parser.rb
@@ -0,0 +1,34 @@
+#CLASS FILE
+#github_url = UrlParser.new "https://github.com/search?q=ruby#stuff"
+
+
+class UrlParser
+    
+def initialize
+    @new.url = UrlParser.new
+    @github_url = UrlParser.new "https://github.com/search?q=ruby#stuff"
+
+class UrlParser 
+    def initialize 
+        new.url = UrlParser.new
+end
+    
+    def initialize
+         @new.url = [ ]
+end
+    
+class UrlParser     
+  def github_url.scheme
+    => "https"
+    github_url.domain
+    => "github.com"
+    github_url.port
+    => "443"
+    github_url.path
+    => "search"
+    github.query_string
+    => {"q" => "ruby"}
+    github_url.fragment_id
+    => "stuff"
+  end
+end
